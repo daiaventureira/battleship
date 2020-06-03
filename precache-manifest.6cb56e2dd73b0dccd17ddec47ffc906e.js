fa3c6f553ef469fe8a448a4865dc1feba383d5bd
Daiane Souza <dai2704@gmail.com>
2020-06-03 00:05:29 -0400

    Updates

---
+++ precache-manifest.6cb56e2dd73b0dccd17ddec47ffc906e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5692b46723722cd4d1b0b87c22e0f85",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "a91aa996a4dc869d52d8",
+    "url": "/battleship/static/css/main.6a56cff3.chunk.css"
+  },
+  {
+    "revision": "a58293af9e3ebbe6dc83",
+    "url": "/battleship/static/js/2.d7b2ff8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/battleship/static/js/2.d7b2ff8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a91aa996a4dc869d52d8",
+    "url": "/battleship/static/js/main.7e604b3b.chunk.js"
+  },
+  {
+    "revision": "69c9cd5fccc908a9fc95",
+    "url": "/battleship/static/js/runtime-main.71c23d45.js"
+  },
+  {
+    "revision": "2b3cf37346f3a1f415390a02dd76ca6f",
+    "url": "/battleship/static/media/texture.2b3cf373.png"
+  }
+]);
